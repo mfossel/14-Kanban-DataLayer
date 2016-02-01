8c8a1afac89daab503d823bf8c6f99d146324dea
Mike Fossel <mikefossel@gmail.com>
2016-02-01 11:54:19 -0800

    first draft

---
+++ 14-KanbanDataLayer/14-KanbanDataLayer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_KanbanDataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           using (var db = new KanbanEntities1())
+            {
+              
+                foreach (var list in db.lists)
+                {
+                    
+                    Console.WriteLine(list.Name + "\n");
+                    foreach (var card in list.Cards)
+                    {
+                        Console.WriteLine("\t" + card.Text);
+                    }
+                    Console.WriteLine("\n");
+                }
+
+
+
+
+
+
+
+
+                Console.ReadLine();
+
+
+
+            }
+        }
+    }
+}
