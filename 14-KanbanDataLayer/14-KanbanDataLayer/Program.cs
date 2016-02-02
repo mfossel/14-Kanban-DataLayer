e73402751c2cd9bc7a03ba5768303b791d153af0
Mike Fossel <mikefossel@gmail.com>
2016-02-01 16:46:10 -0800

    added a wpf version

--- 14-KanbanDataLayer/14-KanbanDataLayer/Program.cs
+++ 14-KanbanDataLayer/14-KanbanDataLayer/Program.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace _14_KanbanDataLayer
 {
     class Program
     {
+       
+
+
         static void Main(string[] args)
         {
            using (var db = new KanbanEntities1())
@@ -24,8 +28,25 @@ namespace _14_KanbanDataLayer
                     Console.WriteLine("\n");
                 }
 
+                Console.WriteLine("Enter the name of the list you want to add: ");
+
+                string listname = Console.ReadLine();
+
+                var newie = db.Set<list>();
+                newie.Add(new list { Name = listname, CreatedDate = DateTime.Now });
+                db.SaveChanges();
 
 
+                foreach (var list in db.lists)
+                {
+
+                    Console.WriteLine(list.Name + "\n");
+                    foreach (var card in list.Cards)
+                    {
+                        Console.WriteLine("\t" + card.Text);
+                    }
+                    Console.WriteLine("\n");
+                }
 
 
 
---
+++ KanbanWPF/KanbanWPF/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace KanbanWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new KanbanEntities())
+            {
+                string data = "";
+                foreach (var list in db.lists)
+                {
+
+                    data += list.Name + "\n";
+                    foreach (var card in list.Cards)
+                    {
+                        data += card.Text;
+                        data += "\n";                   }
+                        data += "\n";
+                }
+                DataBlock.Text = data;
+
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new KanbanEntities())
+            {
+                string listname = NewListBox.Text;
+                var newie = db.Set<list>();
+                newie.Add(new list { Name = listname, CreatedDate = DateTime.Now });
+                db.SaveChanges();
+                NewListBox.Clear();
+                MessageBox.Show("You have added a new list.");
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new KanbanEntities())
+            {
+                int cardlistid = 0 ;
+                string cardname = CardNameBox.Text;
+                string cardlist = CardListBox.Text;
+                var newie = db.Set<Card>();
+                var listadd = db.lists.Where(u => u.Name == cardlist);
+                foreach (var u in listadd)
+                {
+                    cardlistid = u.ListId;
+                }
+
+                newie.Add(new Card { Text = cardname, CreatedDate = DateTime.Now, ListId = cardlistid });
+                db.SaveChanges();
+                NewListBox.Clear();
+                MessageBox.Show("You have added a new card.");
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new KanbanEntities())
+            {
+                string deletecardname = DeleteCardBox.Text;
+                var deletecard = db.Cards.Where(u => u.Text == deletecardname);
+                foreach (var u in deletecard)
+                {
+                    db.Cards.Remove(u);
+                }
+
+                db.SaveChanges();
+                DeleteCardBox.Clear();
+                MessageBox.Show("You have deleted a card.");
+            }
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new KanbanEntities())
+            {
+                string deletelistname = DeleteListBox.Text;
+                var deletelist = db.lists.Where(u => u.Name == deletelistname);
+                foreach (var u in deletelist)
+                {
+                    db.lists.Remove(u);
+                }
+
+                db.SaveChanges();
+                DeleteCardBox.Clear();
+                MessageBox.Show("You have deleted a list");
+
+            }
+        }
+
+       
+    }
+}
